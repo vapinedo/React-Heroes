c325ce327d22bef9f1d196a36d51a90aca052540
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-13 17:11:50 -0500

    HeroCard added, HeroList updated

--- src/app/pages/HeroList.js
+++ src/app/pages/HeroList.js
@@ -1,14 +1,15 @@
 import { getHeroByPublisher } from "@models/HeroModel"
+import { HeroCard } from "@components/HeroCard";
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroByPublisher(publisher);
     
     return (
-        <ul>
+        <div className="row row-cols-1 row-cols-md-4 g-3">
             {heroes.map(hero => (
-                <li key={hero.id}>{hero.superhero}</li>
+                <HeroCard key={hero.id} hero={hero} />
             ))}
-        </ul>
+        </div>  
     )
 }
