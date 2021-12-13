7bbf75f0d60438b623148696bb076995c4052ffe
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-13 14:22:07 -0500

    Login & Logout simple implementation done

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
 
+    const navigate = useNavigate();
+
     const logout = () => {
-        console.log("logout");
+        navigate("/login", { replace: true });
     };
     
     return (
--- src/app/pages/LoginPage.js
+++ src/app/pages/LoginPage.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginPage = () => {
+
+    const navigate = useNavigate();
+
+    const login = () => {
+        navigate("/marvel", { replace: true });
+    };
+    
     return (
-        <div>
-            <h3>Login</h3>
+        <div className='container-lg main-wrapper'>
+            <h3>Login Page</h3>
             <hr />
+
+            <button onClick={login} className="btn btn-primary">Acceder</button>
         </div>
     )
 }
