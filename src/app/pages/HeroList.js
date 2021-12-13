077890779bf97cf2f70c9d2da0cb1da692d6be94
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-13 14:55:50 -0500

    HeroList updated

--- src/app/pages/HeroList.js
+++ src/app/pages/HeroList.js
@@ -5,14 +5,10 @@ export const HeroList = ({ publisher }) => {
     const heroes = getHeroByPublisher(publisher);
     
     return (
-        <>
-            <h3>Hero List - {publisher}</h3>
-
-            <ul>
-                {heroes.map(hero => (
-                    <li key={hero.id}>{hero.superhero}</li>
-                ))}
-            </ul>
-        </>
+        <ul>
+            {heroes.map(hero => (
+                <li key={hero.id}>{hero.superhero}</li>
+            ))}
+        </ul>
     )
 }
