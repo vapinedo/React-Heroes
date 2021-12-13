9c25eea798349ae21b7185cbaafaff0fe2898aa8
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-13 11:00:11 -0500

    Navbar updated

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -2,49 +2,38 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
+
+    const logout = () => {
+        console.log("logout");
+    };
+    
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             
-            <Link 
-                className="navbar-brand" 
-                to="/"
-            >
-                Asociaciones
-            </Link>
+            <Link className="navbar-brand" to="/">Heroes</Link>
 
             <div className="navbar-collapse">
                 <div className="navbar-nav">
 
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/marvel"
-                    >
+                    <NavLink to="/marvel"
+                        className={ ({ isActive }) => "nav-item nav-link " + (isActive ? "active": "") }>
                         Marvel
                     </NavLink>
 
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/dc"
-                    >
+                    <NavLink to="/dc"
+                        className={ ({ isActive }) => "nav-item nav-link " + (isActive ? "active": "") }>
                         DC
                     </NavLink>
                 </div>
             </div>
 
-            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
-                    >
-                        Logout
-                    </NavLink>
+
+                    <span className='nav-item nav-link text-info'>Valp</span>
+                    
+                    <button onClick={ logout }
+                        className="nav-item nav-link btn">Logout</button>
                 </ul>
             </div>
         </nav>
