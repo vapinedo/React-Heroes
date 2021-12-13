8e982d33634d03277c1964bd48b7e970034419c3
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-13 13:59:17 -0500

    Hero comp and DashboarRouter added

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,7 @@ module.exports = function override(config) {
     alias({
         "@app": "src/app",
         "@pages": "src/app/pages",
+        "@routers": "src/app/routers",
         "@components": "src/app/components"
     })(config);
     return config;
