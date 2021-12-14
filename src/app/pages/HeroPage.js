dddd5a4d4e7039098810dc032785499f2e06aed9
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-14 13:52:30 -0500

    HeroPage updated

--- src/app/pages/HeroPage.js
+++ src/app/pages/HeroPage.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import { useParams, Mavigate, Navigate } from "react-router-dom";
+import { useParams, useNavigate, Navigate } from "react-router-dom";
 import { getHeroById } from '@models/HeroModel';
-import { HeroCard } from '@components/HeroCard';
 
 export const HeroPage = () => {
 
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        navigate(-1);
+    };
+
     const { id } = useParams();
     const hero = getHeroById(id);
+    const imagePath = `/assets/${hero.id}.jpg`;
 
     if(!hero) {
         return <Navigate to="/" />
     }
     
     return (
-        <div>
-            <h3>Hero</h3>
-            <hr />
+        <div className='row mt-4'>
+            <div className="col-4">
+                <img src={imagePath} className="img-thumbnail" alt={hero.superhero} />
+            </div>
 
-            {/* <HeroCard hero={hero}> */}
-            {hero.superhero}
+            <div className="col-8">
+                <h4>{hero.superhero}</h4>
+                <ul className="list-group">
+                    <li className="list-group-item"><b>Alter ego:</b> {hero.alter_ego}</li>
+                    <li className="list-group-item"><b>Publisher:</b> {hero.publisher}</li>
+                    <li className="list-group-item"><b>First appearance:</b> {hero.first_appearance}</li>
+                    <li className="list-group-item"><b>Characteres:</b> {hero.characters}</li>
+                </ul>
+
+                <button onClick={ goBack } className="btn btn-outline-info mt-4">Go Back</button>
+            </div>
         </div>
     )
 }
