609c257ef1da87d5871010ee1aa22567fdcff2f8
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-22 16:29:08 -0500

    feat: add public routes

--- src/app/AppRouter.js
+++ src/app/AppRouter.js
@@ -2,12 +2,18 @@ import { LoginPage } from '@pages/LoginPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DashboardRoute } from '@routers/DashboardRoute';
 import { PrivateRoute } from '@routers/PrivateRoute';
+import { PublicRoute } from '@routers/PublicRoute';
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<LoginPage />} />
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage />
+                    </PublicRoute>
+                } />
+
                 <Route path="/*" element={
                     <PrivateRoute>
                         <DashboardRoute />
