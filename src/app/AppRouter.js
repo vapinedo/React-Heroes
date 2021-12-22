dc00cf67074c068365ad493e7374e9184060dc2a
Victor Pinedo <victorpinedomartinez@gmail.com>
2021-12-22 14:58:14 -0500

    feat: add private routes

--- src/app/AppRouter.js
+++ src/app/AppRouter.js
@@ -1,13 +1,18 @@
 import { LoginPage } from '@pages/LoginPage';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { DashboardRoute } from '@routers/DashboardRoute';
+import { PrivateRoute } from '@routers/PrivateRoute';
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
-                <Route path="/*" element={<DashboardRoute />} />
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <DashboardRoute />
+                    </PrivateRoute>
+                } />
             </Routes>
         </BrowserRouter>
     )
